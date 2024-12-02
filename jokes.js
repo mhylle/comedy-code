cd110468fbccac6232bd5dc0142288945680811b
Martin Hylleberg <mhylle@yahoo.com>
2024-12-02 09:18:43 +0100

    Add jokes.js with collection of programming jokes

---
+++ jokes.js
@@ -0,0 +1,62 @@
+const allJokes = [
+    {
+        setup: "Why don't programmers like nature?",
+        punchline: "It has too many bugs!"
+    },
+    {
+        setup: "What did the database administrator say when they got locked out?",
+        punchline: "I need my MySQL password!"
+    },
+    {
+        setup: "Why do JavaScript developers need to wear glasses?",
+        punchline: "Because they don't see sharp!"
+    },
+    {
+        setup: "What's a computer's favorite snack?",
+        punchline: "Microchips!"
+    },
+    {
+        setup: "What do you call a computer that sings?",
+        punchline: "A Dell!"
+    },
+    {
+        setup: "Why do programmers always mix up Halloween and Christmas?",
+        punchline: "Because Oct 31 == Dec 25!"
+    },
+    {
+        setup: "Why do Java developers wear glasses?",
+        punchline: "Because they don't C#!"
+    },
+    {
+        setup: "What's a programmer's favorite hangout spot?",
+        punchline: "Foo Bar!"
+    },
+    {
+        setup: "Why did the programmer quit his job?",
+        punchline: "Because he didn't get arrays!"
+    },
+    {
+        setup: "What do you call a programmer from Finland?",
+        punchline: "Nerdic!"
+    },
+    {
+        setup: "Why was the JavaScript developer sad?",
+        punchline: "Because he didn't know how to 'null' his feelings!"
+    },
+    {
+        setup: "Why did the developer go broke?",
+        punchline: "Because he used up all his cache!"
+    },
+    {
+        setup: "What's a programmer's favorite place in Italy?",
+        punchline: "ROM!"
+    },
+    {
+        setup: "Why do programmers hate nature?",
+        punchline: "It has too many natural bugs!"
+    },
+    {
+        setup: "What did the HTML developer say to the CSS developer?",
+        punchline: "You've got style!"
+    }
+];
